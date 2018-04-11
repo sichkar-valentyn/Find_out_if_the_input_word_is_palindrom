e776ace0b2e4eea41ccd59c6433e0c60aa163edc
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-11 12:21:09 +0300

    Update Find_out_if_the_input_word_is_palindrom.py

--- Find_out_if_the_input_word_is_palindrom.py
+++ Find_out_if_the_input_word_is_palindrom.py
@@ -1,3 +1,15 @@
+# File: Find_out_if_the_input_word_is_palindrom.py
+# Description: Processing string and checking if it is palindrom
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# [1] Valentyn N Sichkar. Find out if the input word is palindrom // GitHub platform [Electronic resource]. URL: https://github.com/sichkar-valentyn/Find_out_if_the_input_word_is_palindrom (date of access: XX.XX.XXXX)
+
+
 # Imlementing the task
 # Find out if the input word is palindrom
 
